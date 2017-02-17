6bd04f3eadcb96e27bfc534444ad5b03a885ea04
BuildTools <ryanhecht.tse@gmail.com>
2017-02-16 21:12:52 -0500

    Added positional input

--- js/main.js
+++ js/main.js
@@ -26,7 +26,9 @@ var mLegRight; //*
 var mArmLeft; //*
 var mArmRight; //*
 var armorstand, armorstandWrapper; //Group all the other elements
-
+var xPos = "~"
+var yPos = "~"
+var zPos = "~"
 
 //DATA -> Stuff that we'll use to generate the command. Fetched from the controls.
 var invisible = false;
@@ -288,9 +290,10 @@ function handleInput(){
 	rightLeg.set(getRangeInput("rightLegX"), getRangeInput("rightLegY"), getRangeInput("rightLegZ"));
 	leftArm.set(getRangeInput("leftArmX"), getRangeInput("leftArmY"), getRangeInput("leftArmZ"));
 	rightArm.set(getRangeInput("rightArmX"), getRangeInput("rightArmY"), getRangeInput("rightArmZ"));
-
 	rotation = getRangeInput("rotation");
-
+	xPos = getInput("x-coord");
+	yPos = getInput("y-coord");
+	zPos = getInput("z-coord")
 	updateUI();
 }
 function getCheckBoxInput(name){
@@ -379,7 +382,10 @@ function updateUI(){
 }
 
 function generateCode(){
-	var code = "/summon ArmorStand ~ ~ ~ {";
+	var code = "/summon ArmorStand ";
+		code += xPos + " " + yPos + " " + zPos + " {";
+
+
 
 	var tags = [];
 
