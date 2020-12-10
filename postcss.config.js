5c5f34dc363b809d379925812cc828ab462cbfaf
Lars Martens <lars@martens.red>
2020-12-10 13:14:25 +0100

    Deleted all old things, added Vue skeleton

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const autoprefixer = require("autoprefixer");
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+    plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+    ]
+}
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Hello Vue!</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
